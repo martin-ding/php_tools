d31fd5d8af745be26612aadb5a0ee1c6c789a7c0
martin-ding <1150354658@qq.com>
2018-11-19 17:47:41 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,32 @@
+<?php
+
+$config['database'] = array(
+        'xxxx' => array(
+            'master' => array(
+                'host' => 'xxxxx',
+                'port' => 3306,
+            ),
+            'slave' => array(
+                'host' => 'xxxxx',
+                'port' => 3306,
+            ),
+            'username' => 'root',
+            'password' => 'xxxxx',
+            'charset' => 'utf8',
+        ),
+        'xq_2345_common' => array(
+            'master' => array(
+                'host' => 'xxxxx',
+                'port' => 3306,
+            ),
+            'slave' => array(
+                'host' => 'xxxx',
+                'port' => 3306,
+            ),
+            'username' => 'root',
+            'password' => 'xxxx',
+            'charset' => 'utf8',
+        )
+);
+
+
